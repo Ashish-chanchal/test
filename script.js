a955a9b9a40c2457a3e14dafdcc159a4a6fd97f7
Ashish Chanchal <akchanchal2002@gmail.com>
2023-04-26 14:48:17 +0000

    add

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+;
+function getval(){
+    let changedata = document.getElementById("changedata").value;
+    changedata = Number(changedata);
+    let valuee=[];
+    if(changedata<0){
+       let stri = "Enter positive value";
+     document.getElementById("change").innerHTML=stri;
+    }
+    else {
+        if(changedata%2==0){
+        let change = document.getElementById("change");
+        valuee.push(changedata+2)
+        valuee.push(changedata+4)
+        valuee.push(changedata+6)
+        change.innerHTML=valuee;
+    }
+    else{
+        let change = document.getElementById("change");
+        valuee.push(changedata+2)
+        valuee.push(changedata+4)
+        valuee.push(changedata+6)
+        change.innerHTML=valuee;
+    }
+
+    }
+}
 
